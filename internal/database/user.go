a6b6a3cccbc447fe3931ece8fbf19470b620a0be
CANITEY <mohamedmed01345@gmail.com>
2024-03-16 00:36:21 +0200

    added function
    functions to add users, and check users' existance

--- internal/database/user.go
+++ internal/database/user.go
@@ -1,1 +1,20 @@
 package database
+
+import "ecommerce/api/models"
+
+func (d *DB) CheckUser(email, password string) (error) {
+	_, err := d.conn.Exec("SELECT email, password FROM users where email=? AND password=?", email, password)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (d *DB) AddUser(user models.User) (error) {
+	_, err := d.conn.Exec("INSERT INTO users(username, password, email, address) VALUES(?,?,?,?)", user.Username, user.Password, user.Email, user.Address)
+	if err != nil {
+		return err
+	}
+	return nil
+}
