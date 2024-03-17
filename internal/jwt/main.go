a60eb926e87d0c63577753c444bfd1930f01cebc
CANITEY <mohamedmed01345@gmail.com>
2024-03-17 17:07:44 +0200

    added a key grap from env

--- internal/jwt/main.go
+++ internal/jwt/main.go
@@ -2,13 +2,16 @@ package jwt
 
 import (
 	"github.com/golang-jwt/jwt"
+	"os"
 )
 
+var key = []byte(os.Getenv("KEY"))
+
 func GenerateJWT(uuid string) (string, error) {
-	token := jwt.New(jwt.SigningMethodEdDSA)
+	token := jwt.New(jwt.SigningMethodHS256)
 	claims := token.Claims.(jwt.MapClaims)
 	claims["uuid"] = uuid
-	secretToken, err := token.SigningString()
+	secretToken, err := token.SignedString(key)
 	if err != nil {
 		return "", err
 	}
