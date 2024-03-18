9ab5f8b99368f889109e66e11f41797306aa4087
CANITEY <mohamedmed01345@gmail.com>
2024-03-18 22:07:13 +0200

    sending the main so the project is runnable for any clonners

--- cmd/ecommerse/main.go
+++ cmd/ecommerse/main.go
@@ -1,138 +1,18 @@
 package main
 
 import (
-	"fmt"
-	"html/template"
 	"log"
-	"net/http"
-
-	"ecommerce/internal/database"
-
-	"github.com/gorilla/mux"
-	"github.com/urfave/negroni"
+	_ "ecommerce/internal/routes"
 )
 
-var d database.DB
 
 func main() {
-	d, err := database.NewConn()
-	if err != nil {
-		panic(err)
-	}
-	
+	defer func() {
+		if r := recover(); r != nil {
+			log.Fatalln(r)
+		}
+	}()
 
-	r := mux.NewRouter()
-	r.HandleFunc("/", home)
-	r.HandleFunc("/products", products)
-	r.HandleFunc("/products/{id}", product)
-	r.HandleFunc("/auth", auth).Methods("GET")
-	r.HandleFunc("/profile", profile)
-	r.HandleFunc("/cart", cart)
-
-	n := negroni.Classic()
-	n.UseHandler(r)
-	if err := http.ListenAndServe(":8888", n); err != nil {
-		log.Fatalln(err)
-	}
 
 }
 
-
-func home(w http.ResponseWriter, r *http.Request) {
-	files := []string{
-		"./web/templates/base.tmpl",
-		"./web/templates/partials/nav.tmpl",
-		"./web/templates/pages/home.tmpl",
-	}
-
-	t, err := template.ParseFiles(files...)
-	if err != nil {
-		panic(err)
-	}
-	err = t.ExecuteTemplate(w, "base", "Hello world!")
-	if err != nil {
-		log.Println(err)
-	}
-
-}
-
-func products(w http.ResponseWriter, r *http.Request) {
-	d.GetProducts()
-	files := []string{
-		"./web/templates/base.tmpl",
-		"./web/templates/partials/nav.tmpl",
-		"./web/templates/pages/products.tmpl",
-	}
-	t, err := template.ParseFiles(files...)
-	if err != nil {
-		panic(err)
-	}
-	err = t.ExecuteTemplate(w, "base", "Hello world!")
-	if err != nil {
-		log.Println(err)
-	}
-}
-
-func product(w http.ResponseWriter, r *http.Request) {
-	files := []string{
-		"./web/templates/base.tmpl",
-		"./web/templates/partials/nav.tmpl",
-		"./web/templates/pages/product.tmpl",
-	}
-	t, err := template.ParseFiles(files...)
-	if err != nil {
-		panic(err)
-	}
-	err = t.ExecuteTemplate(w, "base", "Hello world!")
-	if err != nil {
-		log.Println(err)
-	}
-}
-
-func auth(w http.ResponseWriter, r *http.Request) {
-	files := []string{
-		"./web/templates/base.tmpl",
-		"./web/templates/partials/nav.tmpl",
-		"./web/templates/pages/auth.tmpl",
-	}
-	t, err := template.ParseFiles(files...)
-	if err != nil {
-		panic(err)
-	}
-	err = t.ExecuteTemplate(w, "base", "Hello world!")
-	if err != nil {
-		log.Println(err)
-	}
-}
-
-func profile(w http.ResponseWriter, r *http.Request) {
-	files := []string{
-		"./web/templates/base.tmpl",
-		"./web/templates/partials/nav.tmpl",
-		"./web/templates/pages/profile.tmpl",
-	}
-	t, err := template.ParseFiles(files...)
-	if err != nil {
-		panic(err)
-	}
-	err = t.ExecuteTemplate(w, "base", "Hello world!")
-	if err != nil {
-		log.Println(err)
-	}
-}
-
-func cart(w http.ResponseWriter, r *http.Request) {
-	files := []string{
-		"./web/templates/base.tmpl",
-		"./web/templates/partials/nav.tmpl",
-		"./web/templates/pages/cart.tmpl",
-	}
-	t, err := template.ParseFiles(files...)
-	if err != nil {
-		panic(err)
-	}
-	err = t.ExecuteTemplate(w, "base", "Hello world!")
-	if err != nil {
-		log.Println(err)
-	}
-}
